f9d0b3ef91c182787d80e4772bd68351c81c5938
José Sbaraini <josesbarainips@gmail.com>
2024-04-28 10:57:36 -0400

    Atuzalização

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+<<<<<<< HEAD
 document.getElementById("closeBtn").addEventListener("click", function() {
     document.getElementById("alarm").style.display = "none";
   });
+=======
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("popup").style.display = "flex";
+});
+
+document.getElementById("popup-content").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "none";
+});
+>>>>>>> d77f811 (Atuzalização)
